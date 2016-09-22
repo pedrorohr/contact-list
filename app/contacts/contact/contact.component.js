6f658fe659b4723a004404de0f61aecd6c528354
Pedro Guilherme Rohr <pedro.guilherme.rohr@gmail.com>
2016-09-22 11:13:13 -0300

    Removed the javascript files and node_modules folder from .gitignore

---
+++ app/contacts/contact/contact.component.js
@@ -0,0 +1,39 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var contact_model_1 = require('../shared/contact.model');
+var contact_service_1 = require('../shared/contact.service');
+var ContactComponent = (function () {
+    function ContactComponent(contactService) {
+        this.contactService = contactService;
+    }
+    ContactComponent.prototype.removeContact = function (contact) {
+        this.contactService.removeContact(contact);
+    };
+    ContactComponent.prototype.starContact = function (contact) {
+        this.contactService.starContact(contact);
+    };
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', contact_model_1.Contact)
+    ], ContactComponent.prototype, "contact", void 0);
+    ContactComponent = __decorate([
+        core_1.Component({
+            selector: 'cl-contact',
+            templateUrl: 'app/contacts/contact/contact.component.html',
+            styleUrls: ['app/contacts/contact/contact.component.css']
+        }), 
+        __metadata('design:paramtypes', [contact_service_1.ContactService])
+    ], ContactComponent);
+    return ContactComponent;
+}());
+exports.ContactComponent = ContactComponent;
+//# sourceMappingURL=contact.component.js.map
---
+++ app/contacts/shared/contact.service.js
@@ -0,0 +1,51 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var Subject_1 = require('rxjs/Subject');
+var ContactService = (function () {
+    function ContactService() {
+        this.contacts = [];
+        //Observable sources
+        this.contactAddedSource = new Subject_1.Subject();
+        this.contactRemovedSource = new Subject_1.Subject();
+        this.contactStarredSource = new Subject_1.Subject();
+        //Observable streams
+        this.contactAdded$ = this.contactAddedSource.asObservable();
+        this.contactRemoved$ = this.contactRemovedSource.asObservable();
+        this.contactStarred$ = this.contactStarredSource.asObservable();
+    }
+    ContactService.prototype.getContacts = function () {
+        return this.contacts;
+    };
+    //Service message commands
+    ContactService.prototype.addContact = function (newContact) {
+        this.contacts.push({
+            id: 4,
+            name: newContact,
+            star: false
+        });
+    };
+    ContactService.prototype.removeContact = function (contact) {
+        var index = this.contacts.indexOf(contact);
+        this.contacts.splice(index, 1);
+    };
+    ContactService.prototype.starContact = function (contact) {
+        var index = this.contacts.indexOf(contact);
+        this.contacts[index].star = !this.contacts[index].star;
+    };
+    ContactService = __decorate([
+        core_1.Injectable(), 
+        __metadata('design:paramtypes', [])
+    ], ContactService);
+    return ContactService;
+}());
+exports.ContactService = ContactService;
+//# sourceMappingURL=contact.service.js.map
