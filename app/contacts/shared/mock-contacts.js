e869419d8ba9c13b0be4a51efef096cd60c150ed
Pedro Guilherme Rohr <pedro.guilherme.rohr@gmail.com>
2016-09-29 03:16:47 -0300

    Production mode enabled

---
+++ app/contacts/shared/mock-contacts.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.ContactsDatabase = [
+    { id: 0, firstName: "Pedro", lastName: "Guilherme Rohr", fullName: "Pedro Guilherme Rohr", number: "+55 (51) 99999-9999", email: "pedro.guilherme.rohr@gmail.com", star: true },
+    { id: 1, firstName: "Cristiano", lastName: "Chimango", fullName: "Cristiano Chimango", number: "+55 (51) 12345-6789", email: "adiosmaragatos@gmail.com", star: false },
+    { id: 2, firstName: "Murillo", lastName: "Parede", fullName: "Murillo Parede", number: "+55 (51) 88888-8888", email: "bateevolta@gmail.com", star: false },
+    { id: 3, firstName: "Douglas", lastName: "Ginger Beard", fullName: "Douglas Ginger Beard", number: "+55 (51) 66666-6666", email: "hailragnar@gmail.com", star: false }
+];
+//# sourceMappingURL=mock-contacts.js.map
